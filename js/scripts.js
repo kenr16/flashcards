84522bc1161500580ccb6389c79f11519bc0c6f6
Ken Rutan and Nick Powell <nick@>
2017-03-22 17:39:55 -0700

    Add further cards to the deck for additional learning opportunities.

--- js/scripts.js
+++ js/scripts.js
@@ -3,70 +3,15 @@ $(document).ready(function() {
   $("body").hover(function() {
   });
 
-  // $("div.col-md-3").hover(function() {
-  //   $(this).find("div").fadeToggle();
-  //
+  $("div.col-md-3").hover(function() {
+    $(this).find("div").fadeToggle();
+
+  });
+
+
+  // $(".clickable1").hover(function() {
+  //   $(".card-showing1").fadeToggle(500);
+  //   $(".card-hidden1").fadeToggle(500);
   // });
-
-
-  $(".clickable1").hover(function() {
-    $(".card-showing1").fadeToggle(500);
-    $(".card-hidden1").fadeToggle(500);
-  });
-
-  $(".clickable2").hover(function() {
-    $(".card-showing2").fadeToggle(500);
-    $(".card-hidden2").fadeToggle(500);
-  });
-
-  $(".clickable3").hover(function() {
-    $(".card-showing3").fadeToggle(500);
-    $(".card-hidden3").fadeToggle(500);
-  });
-
-  $(".clickable4").hover(function() {
-    $(".card-showing4").fadeToggle(500);
-    $(".card-hidden4").fadeToggle(500);
-  });
-
-  $(".clickable5").hover(function() {
-    $(".card-showing5").fadeToggle(500);
-    $(".card-hidden5").fadeToggle(500);
-  });
-
-  $(".clickable6").hover(function() {
-    $(".card-showing6").fadeToggle(500);
-    $(".card-hidden6").fadeToggle(500);
-  });
-
-  $(".clickable7").hover(function() {
-    $(".card-showing7").fadeToggle(500);
-    $(".card-hidden7").fadeToggle(500);
-  });
-
-  $(".clickable8").hover(function() {
-    $(".card-showing8").fadeToggle(500);
-    $(".card-hidden8").fadeToggle(500);
-  });
-
-  $(".clickable9").hover(function() {
-    $(".card-showing9").fadeToggle(500);
-    $(".card-hidden9").fadeToggle(500);
-  });
-
-  $(".clickable10").hover(function() {
-    $(".card-showing10").fadeToggle(500);
-    $(".card-hidden10").fadeToggle(500);
-  });
-
-  $(".clickable11").hover(function() {
-    $(".card-showing11").fadeToggle(500);
-    $(".card-hidden11").fadeToggle(500);
-  });
-
-  $(".clickable12").hover(function() {
-    $(".card-showing12").fadeToggle(500);
-    $(".card-hidden12").fadeToggle(500);
-  });
-
+  
 });
